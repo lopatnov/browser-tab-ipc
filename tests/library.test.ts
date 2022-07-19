3b8f3a4931911a2a3854037e0c343165560d5963
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2022-07-19 06:54:04 +0300

    Install @lopatnov/express-reverse-proxy

--- tests/library.test.ts
+++ tests/library.test.ts
@@ -1,26 +1,32 @@
 import path from 'path';
-import { default as puppeteer, Browser, Page } from "puppeteer";
+import {default as puppeteer, Browser, Page} from 'puppeteer';
+import {exec, ChildProcess} from 'node:child_process';
 
 describe('Tests', () => {
-  var browser: Browser;
-  var page1: Page;
-  var page2: Page;
+  let cmd: ChildProcess;
+  let browser: Browser;
+  let page1: Page;
+  let page2: Page;
   const width = 800;
   const height = 600;
-  const relativeUrl = './demo.html';
-  const url = 'file:///' + path.join(__dirname, relativeUrl);
+  const relativeUrl = 'demo.html';
+  const url = 'http://localhost:8080/tests/' + relativeUrl;
+
+  jest.setTimeout(100000);
 
   beforeAll(async () => {
+    cmd = exec('npm start', {cwd: __dirname});
     browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       slowMo: 80,
       timeout: 0,
-      args: [`--window-size=${width},${height}`]
+      args: [`--window-size=${width},${height}`],
+      ignoreDefaultArgs: ['--disable-extensions'],
     });
     page1 = await browser.newPage();
     page2 = await browser.newPage();
-    page1.setViewport({ width, height });
-    page2.setViewport({ width, height });
+    page1.setViewport({width, height});
+    page2.setViewport({width, height});
     await page1.goto(url);
     await page2.goto(url);
   });
@@ -37,21 +43,22 @@ describe('Tests', () => {
       const anchor = document.querySelector('#text') as HTMLInputElement;
       anchor.value = 'A message from page 1';
     });
-    await page1.$eval( '#sendBtn', el => (el as HTMLElement).click() );
+    await page1.$eval('#sendBtn', (el) => (el as HTMLElement).click());
     await page2.waitForSelector('#history li mark.tertiary', {
-      timeout: 3000
+      timeout: 3000,
     });
 
     const receivedElement = await page2.$('#history li span.re');
     await expect(receivedElement).toBeTruthy();
     const text = await page2.evaluate(() => {
       const anchor = document.querySelector('#history li span.re');
-      return anchor.textContent;
+      return anchor?.textContent;
     });
     await expect(text).toBe(message);
   });
 
   afterAll(async () => {
     await browser?.close();
+    cmd.kill();
   });
 });
